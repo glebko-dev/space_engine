7c99de58c7955217c1f53b0f2ea2e2773b46b973
Gleb <glehont@gmail.com>
2025-12-19 01:41:41 +0300

    improved gravitation

--- space_engine_new_new/main.cpp
+++ space_engine_new_new/main.cpp
@@ -144,6 +144,11 @@ public:
 		this->color = color;
 	}
 
+	std::string getName()
+	{
+		return name;
+	}
+
 	Vector3 getPosition()
 	{
 		return position;
@@ -159,6 +164,11 @@ public:
 		return radius;
 	}
 
+	Vector3 getVelocity()
+	{
+		return force;
+	}
+
 	Vector3 getForce()
 	{
 		return force;
@@ -171,15 +181,14 @@ public:
 
 	void tick(float dt)
 	{
-		Vector3 acceleration = { force.x / mass / SCALE, force.y / mass / SCALE, force.z / mass / SCALE };
-		float scaledRadius = radius / SCALE;
+		Vector3 acceleration = { force.x / mass, force.y / mass, force.z / mass };
 
 		velocity = { velocity.x + acceleration.x * dt, velocity.y + acceleration.y * dt, velocity.z + acceleration.z * dt };
 		position = { position.x + velocity.x * dt, position.y + velocity.y * dt, position.z + velocity.z * dt };
 
 		Vector3 scaledPosition = { position.x / SCALE, position.y / SCALE , position.z / SCALE };
 
-		DrawSphereEx(scaledPosition, scaledRadius, SPHERE_RINGS, SPHERE_SLICES, color);
+		DrawSphereEx(scaledPosition, radius / SCALE, SPHERE_RINGS, SPHERE_SLICES, color);
 	}
 };
 
@@ -197,18 +206,20 @@ int main()
 	Object mercury("Mercury", { 0.3f * AU, 0.0f, 0.0f }, 3.33f * (float)pow(10, 23), 2439700.0f, BROWN, { 0.0f, 0.0f, 47360.0f });
 	Object venus("Venus", { 0.71f * AU, 0.0f, 0.0f }, 4.87f * (float)pow(10, 24), 6051800.0f, YELLOW, { 0.0f, 0.0f, -35020.0f });
 	Object earth("Earth", { AU, 0.0f, 0.0f }, 5.9726f * (float)pow(10, 24), 6371000.0f, GREEN, { 0.0f, 0.0f, 29780.0f });
+	//Object moon("Moon", { AU + 384400000.0f, 0.0f, 0.0f}, 7.34f * (float)pow(10, 22), 1738140.0f, GRAY, {0.0f, 0.0f, earth.getVelocity().z + 1023.0f});
 	Object mars("Mars", { 1.4f * AU, 0.0f, 0.0f }, 6.42f * (float)pow(10, 23), 3376200.0f, RED, { 0.0f, 0.0f, 24130.0f });
 
 	Object jupiter("Jupiter", { 4.95f * AU, 0.0f, 0.0f }, 1.9f * (float)pow(10, 27), 71492000.0f , BROWN, { 0.0f, 0.0f, 13070.0f });
 	Object saturn("Saturn", { 9.0f * AU, 0.0f, 0.0f }, 5.7f * (float)pow(10, 26), 60268000.0f, YELLOW, { 0.0f, 0.0f, 9700.0f });
 	Object uran("Uran", { 18.4f * AU, 0.0f, 0.0f }, 5.7f * (float)pow(10, 25), 25559000.0f, BLUE, { 0.0f, 0.0f, 6800.0f });
 	Object neptune("Neptune", { 29.8f * AU, 0.0f, 0.0f }, 1.02f * (float)pow(10, 26), 24764000.0f, RED, { 0.0f, 0.0f, 5430.0f });
-
+	
 	objects.push_back(sun);
 
 	objects.push_back(mercury);
 	objects.push_back(venus);
 	objects.push_back(earth);
+	//objects.push_back(moon);
 	objects.push_back(mars);
 
 	objects.push_back(jupiter);
@@ -222,7 +233,7 @@ int main()
 	
 	while (!WindowShouldClose())
 	{
-		float wheelMove = GetMouseWheelMove(), dt = GetFrameTime();
+		float wheelMove = GetMouseWheelMove(), dt = 1000.0f;
 		Vector2 mouseDelta = GetMouseDelta();
 
 		float cameraSpeed = camera.getSpeed(), cameraAngleSpeed = camera.getAngleSpeed();
@@ -308,9 +319,28 @@ int main()
 				if (dist > objects[i].getRadius() + objects[i].getRadius())
 				{
 					float forceValue = G * objects[i].getMass() * objects[j].getMass() / (dist * dist);
-					Vector3 directionNormalized = { (pos1.x - pos2.x) / dist, (pos1.y - pos2.y) / dist, (pos1.z - pos2.z) / dist };
+					Vector3 direction = { (pos2.x - pos1.x) / dist, (pos2.y - pos1.y) / dist, (pos2.z - pos1.z) / dist }, currForce1 = objects[i].getForce(), currForce2 = objects[j].getForce();
+
+					if (objects[i].getName() == "Moon" && objects[j].getName() == "Earth")
+						std::cout << "E->M " << currForce1.x + direction.x * forceValue << ' ' << currForce1.y + direction.y * forceValue << ' ' << currForce1.z + direction.z * forceValue << std::endl;
+					else if (objects[i].getName() == "Moon" && objects[j].getName() == "Sun")
+						std::cout << "S->M " << currForce1.x + direction.x * forceValue << ' ' << currForce1.y + direction.y * forceValue << ' ' << currForce1.z + direction.z * forceValue << std::endl;
 					
-					objects[j].setForce({ directionNormalized.x * forceValue, directionNormalized.y * forceValue, directionNormalized.z * forceValue });
+					objects[i].setForce(
+						{
+							currForce1.x + direction.x * forceValue,
+							currForce1.y + direction.y * forceValue,
+							currForce1.z + direction.z * forceValue
+						}
+					);
+
+					objects[j].setForce(
+						{
+							currForce2.x - direction.x * forceValue,
+							currForce2.y - direction.y * forceValue,
+							currForce2.z - direction.z * forceValue
+						}
+					);
 				}
 			}
 		}
